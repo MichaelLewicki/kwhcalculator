56587b2ed6e17d31e2f2178782fd7d7243a83234
Michael Lewicki <lewickiclass@gmail.com>
2022-06-14 00:09:24 -0400

    Fix nuevo en validación

--- src/main/java/com/api/kwhcalculator/dto/UsuarioDTO.java
+++ src/main/java/com/api/kwhcalculator/dto/UsuarioDTO.java
@@ -11,17 +11,9 @@ import java.util.Set;
 public class UsuarioDTO {
 
     private Long id;
-    @NotEmpty(message = "el username ingresado no puede estar vacío")
-    @NotNull(message = "el username no debe ser nulo")
-    @Size(min = 5, message = "El username del usuario debería tener al menos 5 carácteres")
     private String username;
-    @NotEmpty(message = "el email ingresado no puede estar vacío")
-    @NotNull(message = "el email no debe ser nulo")
-    @Size(min = 8, message = "El email del usuario debería tener al menos 5 carácteres")
     private String email;
-    @NotEmpty(message = "la password ingresada no puede estar vacía")
-    @NotNull(message = "la password ingresada no debe ser nula")
-    @Size(min = 6, message = "La contraseña del usuario debería tener al menos 6 carácteres")
+
     private String password;
 
     //añadiendo este atributo, al hacer una request se nos mostrará una lista de sectores generales asociadas al usuario
